517f7e4b7f04a425e6a54a9752339f133e57654c
David Musgrove <david@musgroves.us>
2015-09-25 15:06:19 +0100

    Moving documentation to root for gh-pages branch.

--- src/Ipify.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿/*
-    Copyright © 2015 David Musgrove.
-    Licensed under the terms of the MIT License.
-*/
-
-using System.Reflection;
-
-[assembly: AssemblyTitle("Ipify.Tests")]
-[assembly: AssemblyDescription("Unit tets for Ipify.")]
--- src/Ipify/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿/*
-    Copyright © 2015 David Musgrove.
-    Licensed under the terms of the MIT License.
-*/
-
-using System.Reflection;
-
-[assembly: AssemblyTitle("Ipify")]
-[assembly: AssemblyDescription(".NET client library for ipify.org.")]
